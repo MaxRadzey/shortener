2529f1ed471aa6ba36c2eb142579ccec7968f145
Максим Радзей <max.radzey@yandex.ru>
2026-02-26 23:53:24 +0500

    iter23: Добавлены переменные запуска приложения

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/MaxRadzey/shortener/internal/app"
@@ -10,12 +11,40 @@ import (
 	"go.uber.org/zap"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	version := buildVersion
+	date := buildDate
+	commit := buildCommit
+
+	if version == "" {
+		version = "N/A"
+	}
+	if date == "" {
+		date = "N/A"
+	}
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Println("Build version:", version)
+	fmt.Println("Build date:", date)
+	fmt.Println("Build commit:", commit)
+}
+
 // @title        Shortener API
 // @version      1.0
 // @description  API для сокращения URL и управления короткими ссылками.
 // @host         localhost:8080
 // @BasePath     /
 func main() {
+	printBuildInfo()
+
 	AppConfig := config.New()
 
 	config.ParseEnv(AppConfig)
